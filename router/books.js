8281abcbc07409ed7bd63ca8e679af244a698ccc
GentleCold <1952173800@qq.com>
2022-07-21 21:42:33 +0800

    add comment

--- router/books.js
+++ router/books.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router()
 const booksHandler = require('../router_handler/books')
 
-// get data
+// 获取数据
 router.post('/get-data', booksHandler.getData)
+// 爬取数据
 router.post('/crawl-data', booksHandler.crawlData)
+// 删除数据
 router.post('/del-data', booksHandler.delData)
+// 拆分数据
 router.post('/cut-data', booksHandler.cutData)
 
 module.exports = router
